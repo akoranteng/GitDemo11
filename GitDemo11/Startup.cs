dda4640352882e63250123c0646dc20eca4fad80
akoranteng <akoranteng@msn.com>
2019-10-31 16:25:42 -0400

    Step#1 to show Git How

--- GitDemo11/Startup.cs
+++ GitDemo11/Startup.cs
@@ -27,6 +27,7 @@ namespace GitDemo11
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Added more comments
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
